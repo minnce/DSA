bc75a267d9409b03f2b59b7fc30e3749b831dacf
minnce <chrisminn205@gmail.com>
2025-03-27 23:10:46 -0500

    push didn't work

--- src/Data_Structures/LinkedList.hpp
+++ src/Data_Structures/LinkedList.hpp
@@ -8,14 +8,16 @@ template <typename T> class LinkedList
   public:
     LinkedList()
     {
+        head = std::make_shared<Node>(T{});
+        tail = std::make_shared<Node>(T{});
         head->next = tail;
         tail->prev = head;
     }
 
     void add_item(T item)
     {
-        Node *toAdd = new Node(item);
-        Node *temp = tail->prev;
+        auto toAdd = std::make_shared<Node>(item);
+        auto temp = tail->prev.lock();
         temp->next = toAdd;
         toAdd->prev = temp;
         toAdd->next = tail;
@@ -24,10 +26,11 @@ template <typename T> class LinkedList
 
     void print_list()
     {
-        Node *printer = head->next;
+        auto printer = head->next;
         while (printer != tail)
         {
             std::cout << printer->value << " ";
+            printer = printer->next;
         }
         std::cout << "print done!\n";
     }
@@ -36,11 +39,14 @@ template <typename T> class LinkedList
     struct Node
     {
         T value;
-        Node *next;
-        Node *prev;
-    };
+        std::shared_ptr<Node> next;
+        std::weak_ptr<Node> prev;
 
-    Node *head;
-    Node *tail;
+        Node(const T& val) {
+            value = val;
+        }
+    };
+    std::shared_ptr<Node> head;
+    std::shared_ptr<Node> tail;
 };
 } // namespace dataStructures
