04c81ffb076172f91387c7eb899e921590e0c3f3
minnce <chrisminn205@gmail.com>
2025-03-31 02:42:42 -0500

    ringbuffer done

--- src/Data_Structures/Ringbuffer.hpp
+++ src/Data_Structures/Ringbuffer.hpp
@@ -13,37 +13,65 @@ class Ringbuffer
         buffer = new int[initSize];
         head = 0;
         tail = 0;
-        sz = initSize;
+        maxSize = initSize;
+        currSize = 0;
     }
 
     ~Ringbuffer() { delete[] buffer; }
 
     void add_item(unsigned int item)
     {
+        if (currSize == maxSize)
+        {
+            std::cout << "Resizing\n";
+            resize();
+        }
         buffer[head] = item;
-        head = (head + 1) % sz;
-        sz++;
+        head = (head + 1) % maxSize;
+        currSize++;
     }
 
     std::optional<unsigned int> get_item()
     {
-        if (tail == head)
+        if (currSize == 0)
         {
-            std::cout << "No items in ringbuffer\n";
             return std::nullopt;
         }
-        unsigned int toRet = buffer[tail];
-        tail = (tail + 1) % sz;
-        sz--;
+        int toRet = buffer[tail];
+        tail = (tail + 1) % maxSize;
+        currSize--;
         return toRet;
+    }
+
+    void dump_buffer()
+    {
+        for (int i = 0; i < maxSize; i++)
+        {
+            std::cout << buffer[i] << " ";
+        }
+        std::cout << "\n";
     }
 
   private:
     unsigned int head;
     unsigned int tail;
     int *buffer;
-    unsigned int sz;
+    unsigned int currSize;
+    unsigned int maxSize;
 
-    void resize() {}
+    void resize()
+    {
+        int *newBuffer = new int[maxSize * 2];
+        for (int i = 0; i < currSize; i++)
+        {
+            newBuffer[i] = buffer[tail];
+            tail = (tail + 1) % maxSize;
+        }
+        delete[] buffer;
+        buffer = newBuffer;
+        tail = 0;
+        head = maxSize;
+        maxSize *= 2;
+    }
 };
 } // namespace dataStructures
--- tst/test_ringbuffer.cpp
+++ tst/test_ringbuffer.cpp
@@ -9,3 +9,22 @@ TEST(RingbufferTest, NoItemTest)
     auto ring = Ringbuffer(5);
     ASSERT_EQ(std::nullopt, ring.get_item());
 };
+
+TEST(RingbufferTest, GetTset)
+{
+    auto ring = Ringbuffer(100);
+    ring.add_item(1);
+    ASSERT_EQ(1, ring.get_item());
+}
+
+TEST(RingbufferTest, ResizeTest)
+{
+    auto ring = Ringbuffer(2);
+    ring.add_item(1);
+    ring.add_item(2);
+    ring.add_item(3);
+    ASSERT_EQ(ring.get_item(), 1);
+    ASSERT_EQ(ring.get_item(), 2);
+    ASSERT_EQ(ring.get_item(), 3);
+    ASSERT_EQ(std::nullopt, ring.get_item());
+}
